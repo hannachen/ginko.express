179b2f8831625d3d0c25d0f66141ad553b414a0a
Hanna Chen <hanhan@gmail.com>
2024-06-06 18:48:09 -0400

    Tweak styling

--- hosting/app/components/ComingSoon.tsx
+++ hosting/app/components/ComingSoon.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import { Container, Center, Title, Image } from '@mantine/core';
+import { Container, Center, Title, Image, Grid } from '@mantine/core';
 import Countdown, { type CountdownRendererFn } from 'react-countdown';
 import classes from './ComingSoon.module.css';
 
@@ -8,18 +8,18 @@ export default function ComingSoon() {
   const renderer: CountdownRendererFn = ({ days, hours, minutes, seconds }) => {
     return (
       <>
-        <Title order={3} mt={30}>
-          <span>{days} Days</span>
-        </Title>
-        <Title order={3}>
-          <span>{hours} Hours</span>
-        </Title>
-        <Title order={4} mt={15}>
-          <span>{minutes} Minutes</span>
-        </Title>
-        <Title order={4}>
-          <span>{seconds} Seconds</span>
-        </Title>
+        <div className={classes.countdownInfo}>
+          <strong>{days}</strong> Days
+        </div>
+        <div className={classes.countdownInfo}>
+          <strong>{hours}</strong> Hours
+        </div>
+        <div className={classes.countdownInfo}>
+          <strong>{minutes}</strong> Minutes
+        </div>
+        <div className={classes.countdownInfo}>
+          <strong>{seconds}</strong> Seconds
+        </div>
       </>
     );
   };
@@ -27,24 +27,28 @@ export default function ComingSoon() {
   return (
     <Center bg="var(--mantine-color-gray-light)" className="full-height">
       <Container size="md">
-        <div className={classes.inner}>
-          <div className={classes.content}>
+        <div className={classes.heroContainer}>
+          <div className={classes.colTitle}>
             <Title order={1} className="title">
-              <span className={classes.subtitle}>Countdown to</span><br />
+              <span className={classes.subtitle}>Countdown to</span>
+              <br />
               Ginko <span className="white">Anime North 2025</span>
             </Title>
+          </div>
+          <div className={classes.countdown}>
             <Countdown
               date={new Date('May 23, 2025 16:00:00')}
               renderer={renderer}
             />
           </div>
-          <Image
-            src="/images/ginko-anime-north-mascot-square.png"
-            h={250}
-            w="auto"
-            fit="contain"
-            className={classes.image}
-          />
+          <div className={classes.image}>
+            <Image
+              src="/images/ginko-anime-north-mascot-square.png"
+              h="auto"
+              w={250}
+              fit="contain"
+            />
+          </div>
         </div>
       </Container>
     </Center>
